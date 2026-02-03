0734870d1f6f721c59218901a2775406f4f21c62
Getabalewkemaw <getabalewkemaw@gmail.com>
2026-02-03 16:43:27 +0300

    Set up the frontend with the  react/ts and Tailwin

---
+++ client/src/App.tsx
@@ -0,0 +1,11 @@
+
+import './App.css'
+function App() {
+  return (
+    <div className='text-4xl text-center text-green-500 h-screen'>
+      <h1>getabalew kemaw</h1>
+    </div>
+  )
+}
+
+export default App
